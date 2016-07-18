f0ba2fac501e815d4d17e8d7aa407590781139d5
ewkop <bundze@gmail.com>
2016-07-18 17:44:46 +0200

    processing 2015 data

---
+++ 2015_data.R
@@ -0,0 +1,86 @@
+#For the 2015 dataset
+#Separating data into:
+#'komisje' data table <- all local commissions info columns and collective party scores ('Razem...' columns)
+#'wyniki' data table <- basic commissions info and every observation (scores of every single candidate for every single commission)
+
+require(dplyr)
+require(tidyr)
+require(XLConnect)
+require(DBI)
+require(RSQLite)
+
+#variables
+year <- 2015
+path <- "/home/ev/wybory/raw_data/wyniki/"
+liczbaOkregow <- 41
+
+#clean-up stuff for later
+find_unnecessary <- function() {
+  #we need to find the one column after all "Sejm..." columns
+  #and then every next column after 'Razem...' column (except the last one)
+  x <- vector()
+  for (j in 1:(ncol(temp)-1)) {
+    
+    if (names(temp[j]) %>% startsWith('Sejm.')){
+      x <- j+1
+    }
+    
+    if (names(temp[j]) %>% startsWith('Razem.')){
+      x <- c(x, j+1)
+    }
+  }
+  return(x)
+}
+
+#connect to SQLite
+con <- dbConnect(SQLite(), paste0("wyniki", year, ".sqlite3", collapse=''))
+
+#looping through all the .xls files
+for (i in 1:liczbaOkregow) {
+  
+  #get double digit number
+  if (i<10)
+    i<-paste0("0", i, collapse='')
+  
+  #get data from file
+  wb <- loadWorkbook(paste0(path, year, "/", i, ".xlsx", collapse=''))
+  temp <- readWorksheet(wb, sheet=1, header=T)
+  rm(wb)
+ 
+  #find the indices of some unnecessary irregular columns
+  indeksy <- find_unnecessary()
+   
+  #sort the data
+  komisje <- temp %>%
+      select( Nazwa.komisji,
+              Symbol.kontrolny,
+              Gmina,
+              TERYT.gminy,
+              Numer.obwodu,
+              starts_with('Sejm.'),
+              starts_with('Razem.')
+      )
+  
+  wyniki <- temp %>%
+    #watch out for the order! this works only if 'indeksy' comes first
+      select( -indeksy,
+              -starts_with('Sejm.'),
+              -starts_with('Razem.')
+      )
+  
+  rm(temp)
+ 
+  #gather into long data form
+  wyniki <- wyniki %>% gather(Kandydat, Wynik, 6:ncol(wyniki))
+
+  #export data to SQLite database
+  dbWriteTable(con, name=paste0("wyniki", i, collapse=''), val=wyniki)
+  dbWriteTable(con, name=paste0("komisje", i, collapse=''), val=komisje)
+
+  #clean up
+  rm(wyniki)
+  rm(komisje)
+  gc()
+  
+  cat("Finished processing file: ", i, "/", liczbaOkregow, "\n")
+}
