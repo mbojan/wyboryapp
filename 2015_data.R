d66e344352385cac399582258aa22e6a0ba70a72
ewkop <bundze@gmail.com>
2016-07-19 12:13:43 +0200

    fixes

--- 2015_data.R
+++ 2015_data.R
@@ -14,24 +14,6 @@ year <- 2015
 path <- "/home/ev/wybory/raw_data/wyniki/"
 liczbaOkregow <- 41
 
-#clean-up stuff for later
-find_unnecessary <- function() {
-  #we need to find the one column after all "Sejm..." columns
-  #and then every next column after 'Razem...' column (except the last one)
-  x <- vector()
-  for (j in 1:(ncol(temp)-1)) {
-    
-    if (names(temp[j]) %>% startsWith('Sejm.')){
-      x <- j+1
-    }
-    
-    if (names(temp[j]) %>% startsWith('Razem.')){
-      x <- c(x, j+1)
-    }
-  }
-  return(x)
-}
-
 #connect to SQLite
 con <- dbConnect(SQLite(), paste0("wyniki", year, ".sqlite3", collapse=''))
 
@@ -42,13 +24,24 @@ for (i in 1:liczbaOkregow) {
   if (i<10)
     i<-paste0("0", i, collapse='')
   
-  #get data from file
+  #get data from file into 'temp' variable
   wb <- loadWorkbook(paste0(path, year, "/", i, ".xlsx", collapse=''))
   temp <- readWorksheet(wb, sheet=1, header=T)
   rm(wb)
  
   #find the indices of some unnecessary irregular columns
-  indeksy <- find_unnecessary()
+  indeksy <- vector()
+  #we need to find the one column after all "Sejm..." columns
+  #and then every next column after 'Razem...' column (except the last one)
+    
+  for (j in 1:(ncol(temp)-1)) {
+    if (names(temp[j]) %>% startsWith('Sejm.')){
+      indeksy <- j+1
+    }
+    if (names(temp[j]) %>% startsWith('Razem.')){
+      indeksy <- c(indeksy, j+1)
+    }
+  }
    
   #sort the data
   komisje <- temp %>%
@@ -72,6 +65,11 @@ for (i in 1:liczbaOkregow) {
  
   #gather into long data form
   wyniki <- wyniki %>% gather(Kandydat, Wynik, 6:ncol(wyniki))
+  
+  #split names into 'name' and 'surname' columns (assuming candidates have at most 3 first names - a warning will pop up otherwise)
+  wyniki <- wyniki %>% separate(into=c("a", "b", "c", "Nazwisko"), sep="\\.", col=Kandydat, remove = TRUE, fill = "left")
+  wyniki <- wyniki %>% unite (col="Imiona", a, b, c, sep=" ")
+  wyniki$Imiona <- gsub("NA ", "", wyniki$Imiona)
 
   #export data to SQLite database
   dbWriteTable(con, name=paste0("wyniki", i, collapse=''), val=wyniki)
